7a084943bf9be768b78a267132492782c3dd4036
Giovanni <giosave98@gmail.com>
2020-11-24 00:07:00 -0800

    Modify models for apicontrollers individual

--- Controllers/DepartamentoController.cs
+++ Controllers/DepartamentoController.cs
@@ -32,13 +32,13 @@ namespace schoolpractice.Controllers
             }
         }
 
-        [HttpGet("{idDepto}", Name = "GetDepartamento")]
-        public ActionResult Get(string idDepto)
+        [HttpGet("{idDepartamento}", Name = "GetDepartamento")]
+        public ActionResult Get(string idDepartamento)
         {
             try
             {
-                var departamento = context.departamento.FirstOrDefault(m => m.id_depto == idDepto);
-                return Ok(departamento);
+                var departamentoLocal = context.departamento.FirstOrDefault(m => m.id_departamento == idDepartamento);
+                return Ok(departamentoLocal);
             }
             catch (Exception ex)
             {
@@ -51,9 +51,22 @@ namespace schoolpractice.Controllers
         {
             try
             {
+                if (departamento.id_departamento == null || departamento.id_departamento == "")
+                throw new Exception("No puedes ingresar un ID nulo.");
+
+                if (departamento.id_departamento.Length <= 3)
+                throw new Exception("El ID no puede ser menor a 4 digitos.");
+
+                if (departamento.id_departamento == null|| departamento.id_departamento == "")
+                throw new Exception("No puedes ingresar un nombre en nulo.");
+
+                var departamentoLocal = context.departamento.FirstOrDefault(m => m.id_departamento == departamento.id_departamento);
+                if (departamentoLocal != null)
+                throw new Exception("El ID " + departamentoLocal.id_departamento + " ya fue registrado.");
+
                 context.departamento.Add(departamento);
                 context.SaveChanges();
-                return CreatedAtRoute("GetDepartamento", new { idDepto = departamento.id_depto }, departamento);
+                return Ok("Se ha guardado correctamente");
             }
             catch (Exception ex) 
             {
@@ -61,21 +74,23 @@ namespace schoolpractice.Controllers
             }
         }
 
-        [HttpPut("{idDepto}")]
-        public ActionResult Put(string idDepto, [FromBody] Departamento departamento)
+        [HttpPut("{idDepartamento}")]
+        public ActionResult Put(string idDepartamento, [FromBody] Departamento departamento)
         {
             try
             {
-                if (departamento.id_depto == idDepto)
-                {
-                    context.Entry(departamento).State = EntityState.Modified;
-                    context.SaveChanges();
-                    return CreatedAtRoute("GetDepartamento", new { idDepto = departamento.id_depto }, departamento);
-                }
-                else 
-                {
-                    return BadRequest();
-                }
+                if ((idDepartamento == "" || departamento.id_departamento == "") || (idDepartamento == null || departamento.id_departamento  == null))
+                throw new Exception("No puedes enviar enviar una clave vacia");
+
+                if (idDepartamento.Length <= 3 && departamento.id_departamento .Length <= 3)
+                throw new Exception("Has ingresado la clave mal, debe ser de 4 digitos.");
+
+                if (idDepartamento != departamento.id_departamento)
+                throw new Exception("Las claves no corresponden.");
+
+                context.Entry(departamento).State = EntityState.Modified;
+                context.SaveChanges();
+                return Ok("Fue modificado correctamente");
             }
             catch (Exception ex)
             {
@@ -83,22 +98,24 @@ namespace schoolpractice.Controllers
             }
         }
 
-        [HttpDelete("{idDepto}")]
-        public ActionResult Delete(string idDepto)
+        [HttpDelete("{idDepartamento}")]
+        public ActionResult Delete(string idDepartamento)
         {
             try
             {
-                var departamento = context.departamento.FirstOrDefault(d => d.id_depto == idDepto);
-                if (departamento != null)
-                {
-                    context.departamento.Remove(departamento);
-                    context.SaveChanges();
-                    return Ok(idDepto);
-                }
-                else
-                {
-                    return BadRequest();
-                }
+                if (idDepartamento.Length <= 3)
+                throw new Exception("Has ingresado una clave invalida.");
+
+                if (idDepartamento == null || idDepartamento == "" || idDepartamento == "null")
+                throw new Exception("No puedes enviar un registro vacio");
+
+                var departamentoLocal = context.departamento.FirstOrDefault(d => d.id_departamento == idDepartamento);
+                if (departamentoLocal == null )
+                throw new Exception("No se ha encontrado el curso con clave " + idDepartamento);
+
+                context.departamento.Remove(departamentoLocal);
+                context.SaveChanges();
+                return Ok("Se ha eliminado correctamente");
             }
             catch (Exception ex) 
             {
--- Controllers/PuestoController.cs
+++ Controllers/PuestoController.cs
@@ -14,19 +14,19 @@ namespace schoolpractice.Controllers
     public class PuestoController : ControllerBase
     {
         private readonly AppDbContext context;
-        public PuestoController(AppDbContext context) 
+        public PuestoController(AppDbContext context)
         {
             this.context = context;
         }
 
         [HttpGet]
-        public ActionResult Get() 
+        public ActionResult Get()
         {
             try
             {
                 return Ok(context.puesto.ToList());
             }
-            catch (Exception ext) 
+            catch (Exception ext)
             {
                 return BadRequest(ext.Message);
             }
@@ -37,8 +37,8 @@ namespace schoolpractice.Controllers
         {
             try
             {
-                var puesto = context.puesto.FirstOrDefault(m => m.id_puesto == idPuesto);
-                return Ok(puesto);
+                var puestoLocal = context.puesto.FirstOrDefault(m => m.id_puesto == idPuesto);
+                return Ok(puestoLocal);
             }
             catch (Exception ex)
             {
@@ -51,11 +51,24 @@ namespace schoolpractice.Controllers
         {
             try
             {
+                if (puesto.id_puesto == null || puesto.id_puesto == "")
+                throw new Exception("No puedes ingresar un ID nulo.");
+
+                if (puesto.id_puesto.Length <= 3)
+                throw new Exception("El ID no puede ser menor a 4 digitos.");
+
+                if (puesto.nom_puesto == null|| puesto.nom_puesto == "")
+                throw new Exception("No puedes ingresar un nombre en nulo.");
+
+                var puestoLocal = context.puesto.FirstOrDefault(m => m.id_puesto == puesto.id_puesto);
+                if (puestoLocal != null)
+                throw new Exception("El ID " + puesto.id_puesto + " ya fue registrado.");
+
                 context.puesto.Add(puesto);
                 context.SaveChanges();
-                return CreatedAtRoute("GetPuesto", new { idPuesto = puesto.id_puesto }, puesto);
+                return Ok("Se ha registrado Correctamente");
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
@@ -66,16 +79,22 @@ namespace schoolpractice.Controllers
         {
             try
             {
-                if (puesto.id_puesto == idPuesto)
-                {
-                    context.Entry(puesto).State = EntityState.Modified;
-                    context.SaveChanges();
-                    return CreatedAtRoute("Getpuesto", new { idPuesto = puesto.id_puesto }, puesto);
-                }
-                else 
-                {
-                    return BadRequest();
-                }
+                if ((idPuesto == "" || puesto.id_puesto == "") || (idPuesto == null || puesto.id_puesto == null))
+                throw new Exception("No puedes enviar enviar una clave vacia");
+
+                if (idPuesto.Length <= 3 && puesto.id_puesto.Length <= 3)
+                throw new Exception("Has ingresado la clave mal, debe ser de 4 digitos.");
+
+                if (idPuesto != puesto.id_puesto)
+                throw new Exception("Las claves no corresponden.");//ESTA CONDICION NO DEBERIA PASAR POR SI PASA, HAY PROBLEMA EN EL FRONDTEDN
+
+                /*var cursoLocal = context.curso.FirstOrDefault(m => m.id_curso == idCurso);//DA ERROR YA QUE NO PUEDE HACER LA CONSULTA DE OTRA INSTANCIA
+                if (cursoLocal == null)
+                throw new Exception("El registro "+curso.nom_curso+" no existe.");*/
+
+                context.Entry(puesto).State = EntityState.Modified;
+                context.SaveChanges();
+                return Ok("Fue modificado correctamente");
             }
             catch (Exception ex)
             {
@@ -88,19 +107,21 @@ namespace schoolpractice.Controllers
         {
             try
             {
-                var puesto = context.puesto.FirstOrDefault(d => d.id_puesto == idPuesto);
-                if (puesto != null)
-                {
-                    context.puesto.Remove(puesto);
-                    context.SaveChanges();
-                    return Ok(idPuesto);
-                }
-                else
-                {
-                    return BadRequest();
-                }
+                if (idPuesto.Length <= 3)
+                throw new Exception("Has ingresado una clave invalida.");
+
+                if (idPuesto == null || idPuesto == "" || idPuesto == "null")
+                throw new Exception("No puedes enviar un registro vacio");
+
+                var puestoLocal = context.puesto.FirstOrDefault(d => d.id_puesto == idPuesto);
+                if (puestoLocal == null )
+                throw new Exception("No se ha encontrado el curso con clave " + idPuesto);
+
+                context.puesto.Remove(puestoLocal);
+                context.SaveChanges();
+                return Ok("Se ha eliminado correctamente el curso " + puestoLocal.nom_puesto);
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
